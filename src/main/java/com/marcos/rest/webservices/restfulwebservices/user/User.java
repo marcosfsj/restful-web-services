c5f621e78f1508f324f40eea5aee0106615b65c2
marcfernandes <marcfernandes@teksystems.com>
2019-02-26 15:16:13 -0500

    Initial commit

---
+++ src/main/java/com/marcos/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,53 @@
+package com.marcos.rest.webservices.restfulwebservices.user;
+
+import java.util.Date;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+public class User {
+	
+	private Integer id;
+	
+	@Size(min = 3, message = "Name should have at least 3 characters")
+	private String name;
+	
+	@Past
+	private Date birthDate;
+		
+	public User(Integer id, String name, Date birthDate) {
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+	
+}
